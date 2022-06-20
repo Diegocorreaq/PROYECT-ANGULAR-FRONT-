d9568e363fcb5f0a61e6e29396e75799b585d021
Diegocorreaq <diego.corrq@gmail.com>
2022-06-20 17:44:09 -0500

    Generar Boleta

--- src/app/services/boleta.service.ts
+++ src/app/services/boleta.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Servicio } from '../models/servicio.model';
+import { Boleta } from '../models/boleta.model';
 
 const baseURL = "http://localhost:8090/rest/boleta";
 
@@ -11,5 +11,11 @@ const baseURL = "http://localhost:8090/rest/boleta";
 export class BoletaService {
 
   constructor(private http:HttpClient) { }
+  insertaBoleta(data:Boleta):Observable<any>{
+    return this.http.post(baseURL,data);
+  }
+  listaBoleta():Observable<Boleta[]>{
+    return this.http.get<Boleta[]>(baseURL);
+  }
 
 }
